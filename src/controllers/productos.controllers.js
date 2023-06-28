a6209acfde50db4f3858eb90efdf3cef5968e073
Emilse Arias <emilsearias@gmail.com>
2023-06-28 18:27:10 -0300

    agregamos el modelo de producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -2,3 +2,14 @@
 export const controladorPrueba = (req, res) => {
     res.send("Esta es una prueba de mi ruta GET");
   }
+
+export const crearProducto = (req, res) => {
+   try {
+    
+   } catch (error) {
+    console.log(error);
+    res.status(404).json({
+        mensaje: 'Error al intentar crear un producto' 
+    })
+   }
+  }
