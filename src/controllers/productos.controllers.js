6a3c8f3c60acf95d073810df7a42dba88715f5be
Emilse Arias <emilsearias@gmail.com>
2023-06-28 18:39:43 -0300

    agregamos al controlador la funcion par acrear un producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,11 +1,17 @@
+import Producto from "../models/producto";
 
 export const controladorPrueba = (req, res) => {
     res.send("Esta es una prueba de mi ruta GET");
   }
 
-export const crearProducto = (req, res) => {
+export const crearProducto = async (req, res) => {
    try {
-    
+    const productoNuevo = new Producto(req.body);
+    await productoNuevo.save();
+    res.status(201).json({
+        mensaje: 'El producto fue creado correctamente' 
+    })
+
    } catch (error) {
     console.log(error);
     res.status(404).json({
