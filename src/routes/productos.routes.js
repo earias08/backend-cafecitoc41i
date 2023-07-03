e3450dbc38a4bbc70cb08016b8912f15354e1e73
Emilse Arias <emilsearias@gmail.com>
2023-07-03 18:35:27 -0300

    agregamos validaciones de campo vacio y valor numerico en el campo de precio

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -18,7 +18,17 @@ router
     [
       check("nombreProducto")
         .notEmpty()
-        .withMessage("El nombre del producto es obligatorio"),
+        .withMessage("El nombre del producto es obligatorio")
+        .isLength({ min: 2, max: 100 })
+        .withMessage(
+          "El nombre del producto debe contener entre 2 y 100 caracteres"
+        ),
+      check("precio")
+        .notEmpty()
+        .withMessage("El precio es obligatorio")
+        .isNumeric()
+        .withMessage("El precio debe ser un valor numerico")
+        .custom(()=> {})
     ],
     crearProducto
   )
